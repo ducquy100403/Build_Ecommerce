38825d8f87a7747bfe9401424df1c0de3220d729
ducquy100403 <ngducquy104203@gmail.com>
2025-02-13 11:34:47 +0700

    da hoan thanh reponsive cho trang chu

--- assest/script.js
+++ assest/script.js
@@ -1,1 +1,18 @@
 // Script for navigation bar
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('header__navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', (event) => {
+        event.preventDefault(); // Ngăn load lại trang nếu là <a> hoặc button với type mặc định
+        nav.classList.remove('active');
+    });
+}
